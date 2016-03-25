eab27d427efcef2d2956ef13eb5fff1bc147d5a7
bestK1ngArthur <bestK1ngArthur@aol.com>
2016-03-25 14:24:45 +0300

    Initial Commit

---
+++ HackatoN/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HackatoN
+//
+//  Created by Artem Belkov on 25/03/16.
+//  Copyright © 2016 Artem Belkov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
